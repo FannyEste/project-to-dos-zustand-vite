5c3d6f1c25ec4aac71216af3cb76866eb37b5ebd
esstefanny <estefannymorana@gmail.com>
2024-11-20 22:41:31 +0100

    updated componen TaskItem

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,4 +1,30 @@
 import React from 'react';
 import useTaskStore from '../store/taskStore;
 
-const TaskItem => () {}
+const TaskItem => ({ task }) => {
+    const toggleTask = useTaskStore((state) =>state.toggleTask);
+    const removeTask = useTaskStore((state) =state.removeTask);
+
+    return (
+        <li
+        className={`flex justify-between items-center p-2 boder rounded ${
+            task.completed ? 'bg-gray-300 line-through' :'bg-white'
+        }`}
+        >
+            <span
+            onClick={() => toggleTask(task.id)}
+            className="cursor-pointer"
+            >
+                {task.title}
+            </span>
+            <button 
+              onClick={() => removeTask(task.id)}
+              className="text-red-500"
+            >
+              Delete
+            </button>
+        </li>
+    );
+};
+
+export default TaskItem;
